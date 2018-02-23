00b0046aee63dd11c3d6a83eb653265eb794e592
AlonsoZhang <code@zhangwu.tech>
2018-02-23 15:12:58 +0800

    add jump and info

---
+++ jump/index.php
@@ -0,0 +1,16 @@
+<html xmlns="http://www.phpernote.com/php-template/195.html">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>论坛跳转！</title>
+</head>
+
+
+<body>
+<script type="text/javascript">
+
+location="http://www.baidu.com"; //设定跳转的链接地址
+
+</script>
+
+</body>
+</html>
